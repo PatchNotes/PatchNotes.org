05665d4a113814545f3a5a2fb4a178af22273303
Luke Strickland <luke@axxim.net>
2014-01-18 20:49:04 -0600

    Setup database before testing

---
+++ app/config/testing/database.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	'default' => 'mysql',
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'patchnotes_test',
+			'username'  => 'travis',
+			'password'  => '',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+
+	),
+
+);
