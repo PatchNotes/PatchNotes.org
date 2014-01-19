97929c4884a25ecc58becb94a564331e28d1813c
Luke Strickland <luke@axxim.net>
2014-01-19 09:40:29 -0600

    Adding test command.

---
+++ app/commands/TestCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class TestCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'test';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Run the test suite';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire() {
+		system('phpunit');
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments() {
+		return array();
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions() {
+		return array(
+			array('yes', null, InputOption::VALUE_OPTIONAL, 'Say yes to all prompts.', 'yes'),
+		);
+	}
+
+}
