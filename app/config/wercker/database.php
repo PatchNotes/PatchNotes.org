9e8beda725b530e1fb11d80c90d6a63e557d8c39
Luke Strickland <luke@axxim.net>
2014-02-09 16:43:48 -0600

    Let's try getenv instead...

--- app/config/wercker/database.php
+++ app/config/wercker/database.php
@@ -8,11 +8,11 @@ return array(
 
 		'mysql' => array(
 			'driver' => 'mysql',
-			'host' => $_ENV['WERCKER_MYSQL_HOST'],
-			'port' => $_ENV['WERCKER_MYSQL_PORT'],
-			'database' => $_ENV['WERCKER_MYSQL_DATABASE'],
-			'username' => $_ENV['WERCKER_MYSQL_USERNAME'],
-			'password' => $_ENV['WERCKER_MYSQL_PASSWORD'],
+			'host' => getenv('WERCKER_MYSQL_HOST'),
+			'port' => getenv('WERCKER_MYSQL_PORT'),
+			'database' => getenv('WERCKER_MYSQL_DATABASE'),
+			'username' => getenv('WERCKER_MYSQL_USERNAME'),
+			'password' => getenv('WERCKER_MYSQL_PASSWORD'),
 			'charset' => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix' => '',
