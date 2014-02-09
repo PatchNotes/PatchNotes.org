0cd1b9b37b7654e35bd65bdd86d7422ef07f6bb6
Luke Strickland <luke@axxim.net>
2014-02-09 11:03:40 -0600

    Removing debugging code

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,7 +3,6 @@
 class HomeController extends BaseController {
 
 	public function getIndex() {
-		dd(Config::get('patchnotes.aws.queue.pn_immediate'));
 		$newProjects = Project::orderBy('created_at', 'desc')->limit(4)->get();
 
 		return View::make('home', array(
