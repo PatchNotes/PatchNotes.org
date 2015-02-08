29b21e220d0307d59623381d7b0efede0fff1acc
clone1018 <luke@axxim.net>
2015-02-08 14:38:57 -0600

    Adding some config values I missed

--- config/patchnotes.php
+++ config/patchnotes.php
@@ -2,6 +2,16 @@
 
 return array(
 
+    'emails' => [
+        'updates' => [
+            'from' => [
+                'address' => 'updates@patchnotes.org',
+                'name' => 'PatchNotes Update Robot'
+            ]
+        ]
+    ],
+
+
     'tracking' => [
         'ga' => [
             'code' => 'UA-45550761-1'
