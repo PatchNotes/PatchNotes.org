da0fefa9f39cdcdddcea1cf12b98b41928d694c4
clone1018 <luke@axxim.net>
2015-02-08 10:00:01 -0600

    More debugging

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -9,6 +9,7 @@ class ExampleTest extends TestCase {
 	 */
 	public function testBasicExample()
 	{
+		var_dump(scandir(public_path('build/js')));
 		$response = $this->call('GET', '/');
 		echo($response->getContent());
 		$this->assertEquals(200, $response->getStatusCode());
