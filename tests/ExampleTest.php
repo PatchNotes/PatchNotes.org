6bb54e44fa54e8fa2ae011166230dde9af8cdefe
clone1018 <luke@axxim.net>
2015-02-08 09:31:40 -0600

    Adding debugging

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -10,7 +10,7 @@ class ExampleTest extends TestCase {
 	public function testBasicExample()
 	{
 		$response = $this->call('GET', '/');
-
+		var_dump($response);
 		$this->assertEquals(200, $response->getStatusCode());
 	}
 
